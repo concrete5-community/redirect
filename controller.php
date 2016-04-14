c5139809743fc1415041f18d79a8aaeb2c892dba
Michele Locati <michele@locati.it>
2016-04-14 20:29:47 +0200

    v0.9.3

--- controller.php
+++ controller.php
@@ -14,7 +14,7 @@ class Controller extends \Package
 
     protected $appVersionRequired = '5.7.5';
 
-    protected $pkgVersion = '0.9.2';
+    protected $pkgVersion = '0.9.3';
 
     public function getPackageName()
     {
