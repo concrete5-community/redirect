846ab73a773962cee4b122619b3aa120afe1c5fb
Michele Locati <michele@locati.it>
2021-06-01 14:25:19 +0200

    Version 2.3.1

--- controller.php
+++ controller.php
@@ -22,7 +22,7 @@ class Controller extends Package implements ProviderInterface
      *
      * @var string
      */
-    protected $pkgVersion = '2.3.0';
+    protected $pkgVersion = '2.3.1';
 
     /**
      * The minimum concrete5 version.
